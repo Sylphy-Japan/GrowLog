165e21f258d00de1cb5d1ea55a8cb02d7d52ae7e
Sylphy-Japan <eran.code@gmail.com>
2025-09-17 22:03:57 +0900

    DateCounterクラスとTaskクラスを追加

---
+++ Core/DateCounter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrowLog.Core
+{
+    public class DateCounter
+    {
+        public static int DaysSince(DateOnly from)
+        {
+            var today = DateOnly.FromDateTime(DateTime.Now);
+            return today.DayNumber - from.DayNumber;
+        }
+    }
+}
