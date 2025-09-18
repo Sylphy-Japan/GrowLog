3b5a56e86cbba2ca011dad264ba8df0909e4d8d2
Sylphy-Japan <eran.code@gmail.com>
2025-09-18 23:30:17 +0000

    SQLIghtの最小機能の明記

---
+++ Model/SQLightHelper.cs
@@ -0,0 +1,28 @@
+
+//最初に一度だけテーブルを作る(TaskItem)
+CREATE TABLE IF NOT EXISTS TaskItem(
+    Id      INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name   TEXT NOT NULL,
+    Overview TEXT,
+    Day TEXT,
+    Status  INTEGER NOT NULL
+);
+
+//最初に一度だけテーブルを作る(Taskdiary)
+CREATE TABLE IF NOT EXISTS Taskdiary(
+    
+
+
+)
+
+//データを全て表示
+
+
+//データを挿入する
+
+
+//データを変更する
+
+
+
+//データを消す
